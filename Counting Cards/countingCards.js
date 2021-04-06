c6715612f1afd643228d0b2cc8c6f12a6d9a9657
Tetiana Trofymets <tetiana.trofymets@gmail.com>
2021-04-07 00:05:31 +0300

    Add initial conditions

---
+++ Counting Cards/countingCards.js
@@ -0,0 +1,8 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/counting-cards
+let  count = 0;
+
+function cc(card) {
+  return "Change Me";
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
