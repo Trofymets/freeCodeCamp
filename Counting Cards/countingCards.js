451cc38c3115f1094dd808df02d68f2c4f3aefdd
Tetiana Trofymets <tetiana.trofymets@gmail.com>
2021-04-07 00:31:43 +0300

    Update cc function

--- Counting Cards/countingCards.js
+++ Counting Cards/countingCards.js
@@ -2,7 +2,28 @@
 let  count = 0;
 
 function cc(card) {
-  return "Change Me";
+  switch(card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count--;
+      break;
+    default:
+      break;
+  }
+  if (count <= 0) {
+    return `${count} Hold`;
+  }
+  return `${count} Bet`;
 }
 
 cc(2); cc(3); cc(7); cc('K'); cc('A');
