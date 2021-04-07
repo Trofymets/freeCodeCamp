957b0277dbd8885b7fdd218304077a3d03a7d170
Tetiana Trofymets <tetiana.trofymets@gmail.com>
2021-04-07 14:12:56 +0300

    Add checkObj function

---
+++ checkObj.js
@@ -0,0 +1,7 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/testing-objects-for-properties#
+function checkObj(obj, checkProp) {
+    if(obj.hasOwnProperty(checkProp)) {
+      return obj.[checkProp];
+    }
+    return "Not Found";
+  }
