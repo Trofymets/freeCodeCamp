6e04dbe3d0383b9d73cdab4f6b7c61c6820e0ea3
Tetiana Trofymets <tetiana.trofymets@gmail.com>
2021-04-12 23:52:34 +0300

    Add array sum by for loop

---
+++ arrSumByForLoop.js
@@ -0,0 +1,8 @@
+var myArr = [ 2, 3, 4, 5, 6];
+
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+    // console.log('before', total);
+  total +=  myArr[i];
+// console.log('after', total);
+}
