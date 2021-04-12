d90b3b0dd048a7d26766af124bbef6b2842a6070
Tetiana Trofymets <tetiana.trofymets@gmail.com>
2021-04-13 00:39:45 +0300

    Add checkSign function

---
+++ checkSign.js
@@ -0,0 +1,8 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-multiple-conditional-ternary-operators
+function checkSign(num) {
+    return (num > 0) ? "positive"
+    : (num < 0) ? "negative"
+    : "zero";
+    }
+    
+    checkSign(10);
