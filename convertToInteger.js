348fe2a65fc98bb2103e9f0aa2baebfdc296fec2
Tetiana Trofymets <tetiana.trofymets@gmail.com>
2021-04-16 17:36:41 +0300

    Add convertToInteger func

---
+++ convertToInteger.js
@@ -0,0 +1,7 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-the-parseint-function
+function convertToInteger(str) {
+    return parseInt(str)
+    // return +str
+    }
+    
+    convertToInteger("56");
