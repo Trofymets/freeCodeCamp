b4503798872e5d01bf674c5bdaf78186b923068a
Tetiana Trofymets <tetiana.trofymets@gmail.com>
2021-04-16 17:50:01 +0300

    Add convertToInteger func with Radix

---
+++ convertToInteger(radix).js
@@ -0,0 +1,6 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-the-parseint-function-with-a-radix
+function convertToInteger(str, radix) {
+    return parseInt(str, 2)
+    }
+    
+    convertToInteger("10011");
