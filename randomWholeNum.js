5f218352ec2f844862fc3054412f896382612050
Tetiana Trofymets <tetiana.trofymets@gmail.com>
2021-04-16 17:21:47 +0300

    Add randomWholeNum function

---
+++ randomWholeNum.js
@@ -0,0 +1,5 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/generate-random-whole-numbers-with-javascript
+function randomWholeNum() {
+
+    return Math.floor(Math.random()*10);
+  }
